563ab3f35558ea262e4dd83a4f4c486a86320c83
haurenburu <haurenburu@protonmail.com>
2020-11-14 12:37:05 -0300

    i was using >= instead of just >

--- algorithms/bubblesort.js
+++ algorithms/bubblesort.js
@@ -3,7 +3,7 @@ function bubblesort(data) {
 
     for (let i = 0; i < len; i++) {
         for (let j = 0; j < len - i - 1; j++) {
-            if (data[j] >= data[j + 1]) {
+            if (data[j] > data[j + 1]) {
                 let aux = data[j];
                 data[j] = data[j + 1];
                 data[j + 1] = aux;
